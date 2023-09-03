0b7649e63046a290aa4dd7fd426685656d5036b2
LIULUJIA2022 <115575414+LIULUJIA2022@users.noreply.github.com>
2023-09-03 18:13:28 +0100

    Update Plot_Domain_Name_Classification.py

--- Plot_Domain_Name_Classification.py
+++ Plot_Domain_Name_Classification.py
@@ -1,4 +1,3 @@
-# 导入必要的库
 import matplotlib.pyplot as plt
 
 devices = ['Apple_tv', 'arlo_camera_pro4', 'blink_doorbell', 'boifun_baby',
@@ -10,14 +9,14 @@ devices = ['Apple_tv', 'arlo_camera_pro4', 'blink_doorbell', 'boifun_baby',
            'weekett_kettle','withings_scale','wiz_smart_bulb','wyze_cam_pan_v2'] 
 third_party_dns_counts = [4, 0, 0, 2, 5, 5, 0, 5, 3, 4,3, 0, 0, 0, 32, 2, 2, 2, 4, 5, 0, 2, 39, 0, 0, 0, 10]
 
-# 绘制柱状图
-plt.figure(figsize=(15,7))  # 设置图像大小
-plt.bar(devices, third_party_dns_counts, color='skyblue')  # 绘制柱状图
+# Draw a bar chart
+plt.figure(figsize=(15,7))  
+plt.bar(devices, third_party_dns_counts, color='skyblue') 
 plt.title("Number of Third-party DNS Connections by IoT Device")
 plt.xlabel("IoT Devices")
 plt.ylabel("Number of Third-party DNS")
-plt.xticks(rotation=45)  # 旋转X轴标签使其更易读
-plt.tight_layout()  # 确保所有标签都适应图形窗口
+plt.xticks(rotation=45) 
+plt.tight_layout() 
 plt.show()
 
 
@@ -25,7 +24,7 @@ plt.show()
 labels = ['>10 Third-party DNS', '0-10 Third-party DNS', 'No Third-party DNS']
 sizes = [2, 15, 10]
 colors = ['#ff9999', '#66b2ff', '#ffcc99']
-explode = (0.1, 0.1, 0)  # 突出显示前两个切片
+explode = (0.1, 0.1, 0)  # Highlight the first two slices
 
 plt.figure(figsize=(10,7))
 plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
