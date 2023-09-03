15106bcc9d786938781fd856ef3c93ffb29007bc
LIULUJIA2022 <115575414+LIULUJIA2022@users.noreply.github.com>
2023-09-03 18:12:41 +0100

    Update Plot_Unencrypted_Messages_Percentage.py

--- Plot_Unencrypted_Messages_Percentage.py
+++ Plot_Unencrypted_Messages_Percentage.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# 27个设备的名称和未加密消息的百分比分别存储在以下两个列表中
+# The names of the 27 devices and the percentage of unencrypted messages are stored in the following two lists respectively
 devices = ['Apple_tv', 'arlo_camera_pro4', 'blink_doorbell', 'boifun_baby',
            'bose_speaker','echodot4','eufy_robovac','furbo_dog_camera',
            'google_nest_doorbell','google_nest_hub','govee_strip_light','lifx_mini',
@@ -12,12 +12,12 @@ devices = ['Apple_tv', 'arlo_camera_pro4', 'blink_doorbell', 'boifun_baby',
 percentages = ["1.49%", "0.04%", "0.00%", "0.07%", "1.95%", "0.01%", "0.01%", "1.22%", "0.07%", "0.69%", "0.29%", "0.05%", "0.14%", "0.03%",
                "0.03%", "0.16%", "0.01%", "0.07%", "32.57%", "0.09%", "0.11%", "0.51%", "0.15%", "0.01%", "0.00%", "0.10%", "4.34%"]
 
-# 转换percentages为小数值
+# Convert percentages to decimal values
 percent_values = [float(percentage.strip('%'))/100 for percentage in percentages]
 
 plt.figure(figsize=(15, 8))
 
-# 创建柱状图
+# Create a histogram
 plt.bar(devices, percent_values, color='skyblue')
 plt.ylabel('Unencrypted Messages Percentage')
 plt.xlabel('Devices')
@@ -27,14 +27,14 @@ plt.tight_layout()
 plt.show()
 
 
-# 创建小提琴图
+# Create a violin plot
 sns.violinplot(x=devices, y=percent_values, palette="pastel")
 
 plt.ylabel('Unencrypted Messages Percentage')
 plt.xlabel('Devices')
 plt.title('Distribution of Unencrypted Messages by Device')
-plt.xticks(rotation=90)  # 这将设备名称旋转90度，使其更容易阅读
-plt.tight_layout()  # 确保标签不会被截断
+plt.xticks(rotation=90) 
+plt.tight_layout()
 plt.show()
 
 plt.figure(figsize=(15, 8))
